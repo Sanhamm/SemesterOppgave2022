e2f1ecad34cc21bad22a4e6821c936feee714b28
Sanhamm <sanhamm097@gmail.com>
2022-11-22 14:50:14 +0100

    started on tailwind, made the header and starter slightly on the rest

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      "header-col": "#FFDF8C",
+      "auction-col": "#FF8C8C",
+      "bidding-col": "#08DD2A",
+    },
+    fontFamily: {
+      "header-font": ["karivanar", '"Josefin Sans"', "Verdana"],
+      "body-font": ['"Josefin Sans"', '"Open Sans"', "Verdana"],
+    },
     extend: {},
   },
   plugins: [],
